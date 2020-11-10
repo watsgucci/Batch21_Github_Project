184310f119afd2d215c211cfe8af536b7d50c75c
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 12:55:22 -0500

    added a new class and pushing to remote

---
+++ src/AfterPublish_Practice2.java
@@ -0,0 +1,5 @@
+public class AfterPublish_Practice2 {
+    public static void main(String[] args) {
+        System.out.println("We have published to remote and now we are committing to remote");
+    }
+}
