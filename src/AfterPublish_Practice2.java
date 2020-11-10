a8edacd3d81a238f53558e8132bc2b4f6e32dc5a
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-10 14:14:13 -0500

    adding comments

--- src/AfterPublish_Practice2.java
+++ src/AfterPublish_Practice2.java
@@ -1,5 +1,7 @@
 public class AfterPublish_Practice2 {
     public static void main(String[] args) {
         System.out.println("We have published to remote and now we are committing to remote");
+        //hello
+        //just a commit test
     }
 }
